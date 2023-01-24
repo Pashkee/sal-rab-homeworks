29d8995de7afbeb01e6a67edbb600e1f67c7a922
Neto test <pashkee@yandex.ru>
2023-01-24 15:31:43 +0400

    task final done correct

--- task_final/solution.js
+++ task_final/solution.js
@@ -39,8 +39,8 @@ function sendRequest(name, phone, address, goods, sum) {
     let countOfGoods = goods.length;
 
  for (let i = 0; i < countOfGoods; i += 1) {
-    goods = {title: goods[i].title, count: goods[i].count} 
-    data.goods.push(goods)   
+    let obj = {title: goods[i].title, count: goods[i].count};
+    data.goods.push(obj);   
     }
     
     data.order = {address: "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв. " + address.flat, sum: sum
